77b7bfc8fd47299bd9d74af31606cd54fa4ec83f
Gennady Klyuchnikov <lonely.heart.99@mail.ru>
2022-09-29 23:08:51 +0300

    finish work

---
+++ script/index.js
@@ -0,0 +1,35 @@
+let popup = document.querySelector('.popup')
+let buttonEdit = document.querySelector('.button__edit');
+let buttonClose = document.querySelector('.popup__close');
+let popupButton = document.querySelector('.popup__button');
+let formElement = document.querySelector('.popup__inputs');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__subtitle');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+function popupOpen () {
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+};
+
+buttonEdit.addEventListener('click', popupOpen);
+
+function popupClose () {
+  popup.classList.remove('popup_opened');
+};
+
+buttonClose.addEventListener('click', popupClose);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+
+  popupClose();
+};
+
+formElement.addEventListener('submit', formSubmitHandler);
